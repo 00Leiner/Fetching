7db64edb2bfeff07edf64b0df4fc54b4c2748a7c
Leiner <leinersacdalan4@gmail.com>
2023-11-25 06:16:54 +0800

    users

--- src/api/Users.ts
+++ src/api/Users.ts
@@ -10,13 +10,36 @@ const createDataURL = `${baseUrl}${createData}`;
 const updateDataURL = `${baseUrl}${updateData}`;
 const deleteDataURL = `${baseUrl}${deleteData}`;
 
-export const readUserData = async (): Promise<Users[]> => {
-    try{
+export const readUserData = async (): Promise<Users[] | undefined> => {
+    try {
         const response: AxiosResponse<Users[]> = await axios.get(readDataURL);
-        console.log(response.data)
-        return response.data;
+        if (response.status === 200) {
+            return response.data;
+        } else if (response.status === 404) {
+            console.log('No User Found');
+            throw response.status;
+        } else{
+            console.log('Unexpected Status:', response.status);
+            throw new Error(`Unexpected Status: ${response.status}`);
+        }
     } catch (error) {
         console.error('API Error:', error);
         throw error;
-     }
+    }
+}
+
+export const createUserData = async (userData: Users): Promise<AxiosResponse> => {
+    try {
+        const response = await axios.post(createDataURL , userData);
+        if (response.status === 201) {
+            const responseDataAsString = JSON.stringify(response.data);
+            return responseDataAsString;
+        } else {
+            console.log('Creating Unsuccessful');
+            return new Error(`Creating Unsuccessful`);
+        }
+    } catch (error) {
+        console.error('API Error:', error);
+        throw error;
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
-import {readUserData} from './api/Users';
+import { readUserData, createUserData } from './api/Users';
+import { Users } from './models/Users';
 
-readUserData()
+
+async function fetchDataAndPrint() {
+    try {
+        const userData = await readUserData();
+        console.log(userData);
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+    }
+}
+
+  const handleCreateUser = async () => {
+    const newUser: Users = {
+      username: 'sdasd',
+      password: '123',
+    };
+    try {
+      const response = await createUserData(newUser);
+      console.log('User created successfully:', response.data);
+    } catch (error) {
+      console.error('Failed to create user:', error);
+    }
+  };
+
+handleCreateUser()
